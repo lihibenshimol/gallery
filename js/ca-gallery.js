0b72ab03d87bdd0a8702c31a81f513f564b25b56
Lihi Ben Shimol <lihibs26@gmail.com>
2022-12-05 22:41:43 +0200

    custom portfolio

--- js/ca-gallery.js
+++ js/ca-gallery.js
@@ -1,8 +1,69 @@
-(function($) {
-  "use strict"; // Start of use strict
+'use stirct'
+
+function onInit() {
+  renderProj()
+}
+
+function renderProj() {
+  const projects = getProj()
+  var strHtmls = projects.map(proj => `
+  <div class="col-md-4 col-sm-6 portfolio-item">
+  <a onclick="onOpenModal('${proj.id}')" class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+   <div class="portfolio-hover">
+   <div class="portfolio-hover-content">
+   <i class="fa fa-plus fa-3x"></i>
+   </div>
+   </div>
+   <img class="img-fluid" src="img/portfolio/${proj.id}.png" alt="${proj.name}">
+   </a>
+   <div class="portfolio-caption">
+   <h4>${proj.name}</h4>
+   <p class="text-muted">${proj.title}</p>
+ </div>
+  `
+  )
+  $('.proj-container').html(strHtmls)
+}
+
+
+function onOpenModal(projId) {
+  console.log('hello');
+  var proj = getProjById(projId)
+  console.log('proj = ', proj)
+  var strHtml = `
+              <!-- Project Details Go Here -->
+              <h2>${proj.name}</h2>
+              <p class="item-intro text-muted">${proj.title}</p>
+              <img class="img-fluid d-block mx-auto" src="img/portfolio/${proj.id}.png" alt="${proj.name}">
+              <p>${proj.desc}</p>
+              <ul class="list-inline">
+                <li>Published at ${getDate(proj.publishedAt)}</li>
+                <li>Category: ${proj.lables}</li>
+              </ul>
+              <button class="btn btn-primary" data-dismiss="modal" type="button">
+                  <i class="fa fa-times"></i>
+                  Close Project</button>
+  `
+
+  $('.modal-body').html(strHtml)
+}
+
+function sendEmail() {
+  var email = $('.email').val()
+  var subject = $('.subject').val()
+  var msg = $('.msg').val()
+  var url = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${msg}  `
+  window.open(url, '_blank')
+
+}
+
+
+
+(function ($) {
+  'use strict'
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +77,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -27,7 +88,7 @@
   });
 
   // Collapse the navbar when page is scrolled
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
--- js/main.js
+++ js/main.js
@@ -1,1 +1,78 @@
+'use strict'
+
 console.log('Starting up');
+
+const STORAGE_KEY = 'projectsDB'
+
+const gProjs = [
+    {
+        id: 'pacman',
+        name: 'Pacman',
+        title: 'The classic Pacman game',
+        desc: 'As a part of the bootcamp we learned how to create different board games.',
+        url: 'lihibenshimol.github.io/pacman-ca/',
+        publishedAt: 1669118852000,
+        lables: ['matirx', 'keyboard events', 'board games']
+    },
+    {
+        id: 'book-shop',
+        name: 'Book Shop',
+        title: 'Create your own book shop!',
+        desc: 'My own book-shop project as part of MVC lessons',
+        url: 'lihibenshimol.github.io/book-shop/',
+        publishedAt: 1670236232000,
+        lables: ['mvc', 'data base']
+    },
+    {
+        id: 'mine-sweeper',
+        name: 'Mine Sweeper',
+        title: 'The Classic Mine Sweeper in a new version',
+        desc: 'My first sprint project as part of Coding Academy bootcamp.',
+        url: 'lihibenshimol.github.io/mine-sweeper/',
+        publishedAt: 1669559432000,
+        lables: ['matrix', 'board games']
+    },
+    {
+        id: 'todo',
+        name: 'Todo List',
+        title: 'Get all your TODOs all sort together',
+        desc: 'A todo-list project I improved',
+        url: 'lihibenshimol.github.io/TODO/',
+        publishedAt: 1670254232000,
+        lables: ['mvc', 'data base']
+    },
+    {
+        id: 'guess-me',
+        name: 'Guess Me',
+        title: 'Let the gini read your mind',
+        desc: 'The famous Akinator',
+        url: 'lihibenshimol.github.io/Guess-me/',
+        publishedAt:1670263819000,
+        lables: ['mvc','data base']
+    }
+]
+
+function getProj() {
+    return gProjs
+}
+
+
+function getProjById(projId) {
+    const project = gProjs.find(project => project.id === projId)
+    return project
+}
+
+function getDate(projTimestamp) {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    var date = new Date(projTimestamp)
+    var month = date.getMonth()
+    var year = date.getFullYear()
+    var str = `${months[month + 1]} ${year}`
+        return str
+}
+
+
+
+function _saveProjToStoarge() {
+    saveToStorage(STORAGE_KEY, gProjs)
+}
